7fbe7e9a1953223ef8b6fb21b335fa224f81b4dc
MAO <45424931+marcoamarooliveira@users.noreply.github.com>
2024-05-28 22:25:03 +0100

    update to React 18

--- react_example_04-Children/recipe.js
+++ react_example_04-Children/recipe.js
@@ -1,4 +1,4 @@
 'use strict';
 const e = React.createElement;
 
-ReactDOM.render(e(Recipe), document.getElementById('react-container'));
+reactDOM.createRoot(e(Recipe), document.getElementById('react-container'));
